1a42d141eb67551be54f8a90a6ffaf3876808387
murathe <isaacsm07p@gmail.com>
2020-02-02 22:45:50 +0300

    import models

--- picha/views.py
+++ picha/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime as dt
+from .models import *
 
 # Create your views here.
+def home(request):
+    return HttpResponse('Our pictures goes here!')
