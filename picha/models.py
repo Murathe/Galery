0a0520ac2d53f18b026431c576804d80b77c07c3
murathe <isaacsm07p@gmail.com>
2020-02-02 22:28:13 +0300

    Save func

--- picha/models.py
+++ picha/models.py
@@ -20,4 +20,11 @@ class Category(models.Model):
 class Location(models.Model):
     name = models.CharField(max_length = 20)
 
-    
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
