f1ae9046b8b89a7c74231799dba3843a21bc97b0
murathe <isaacsm07p@gmail.com>
2020-02-03 23:31:16 +0300

    Three models tests

--- picha/tests.py
+++ picha/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
 
 # Create your tests here.
+class ImageTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    # New image
+    self.new_image = Image(title='title1',
+        description='Test image')
+        self.new_image.save()
+
+    # Creating a new location
+        self.new_location = Location(name='nai')
+        self.new_location.save()
+    
+    # Creating a new category
+        self.new_category = Category(name='code')
+        self.new_category.save()
+
+    def tearDown(self):
+        Category.objects.all().delete()
+        Location.objects.all().delete()
+        Image.objects.all().delete()
+
+class LocationTestClass(TestCase):
+
+    """
+    A test that checks for the behaviour of the image class
+    """
+
+    def setUp(self):
+        pass
+
+class CategoryTestClass(TestCase):
+    """
+    A test  that checks the functionality of the image class
+    """
+
+    def setUp(self):
+        pass
