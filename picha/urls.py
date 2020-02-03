d92605572dcd6e2c69a05b2b0a0924551b4fa19b
murathe <isaacsm07p@gmail.com>
2020-02-03 04:25:15 +0300

    Paths mapping

--- picha/urls.py
+++ picha/urls.py
@@ -1,3 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+
+urlpatterns = [
+    url(r"^/$", .as_view(), name=""),
+    url(r"^/$", .as_view(), name=""),
+    url(r"^/$", .as_view(), name=""),
+    
+]
