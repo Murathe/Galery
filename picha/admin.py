ba6ee46d632dda8c7940a4368d8f53d087e24899
murathe <isaacsm07p@gmail.com>
2020-02-03 10:02:31 +0300

    Add models to admin page

--- picha/admin.py
+++ picha/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-# admin.site.register(Location)
-# admin.site.register(Category)
-# admin.site.register(Image)
+admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(Image)
